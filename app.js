3aec32d2a92532c99121a71b8a2ccc415f56733c
Asghar Ali <asgharali.224415@gmail.com>
2024-08-11 15:43:36 +0500

    daf

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://smit-admissionportal-student.vercel.app'
+    'https://smit-admissionportal-student.vercel.app/'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
